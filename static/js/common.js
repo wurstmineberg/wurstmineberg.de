ec79fc80196cf41ade3389e7041ef065b42e119d
farthen <monkey@farthen.de>
2013-11-02 17:01:40 +0100

    Fix the funding script

--- static/js/common.js
+++ static/js/common.js
@@ -109,33 +109,36 @@ function display_funding_data() {
             $('.funding-progressbar').empty();
             var funding_total = 0.0;
             data['history'].forEach(function(transaction) {
-                if (transaction['type'] != 'nessus-monthly') {
-                    funding_total += transaction['amount'];
-                }
+            	funding_total += transaction['amount'];
             });
-            var year = 2013;
-            var month = 9;
-            while (funding_total >= Math.abs(data['spending_monthly'])) {
+
+            var today = new Date();
+            var year = today.getFullYear();
+            var month = today.getMonth();
+            var spending_monthly = Math.abs(data['spending_monthly']);
+
+            while (funding_total >= spending_monthly) {
                 month++;
                 if (month > 12) {
                     year++;
                     month = 1;
                 }
-                funding_total -= Math.abs(data['spending_monthly']);
+
+                funding_total -= spending_monthly;
             }
+
             var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-            if (month == 12) {
-                $('.funding-month').html('December ' + year + ' to January ' + (year + 1));
-            } else {
-                $('.funding-month').html(months[month - 1] + ' to ' + months[month] + ' ' + year);
-            }
+            $('.funding-month').html(months[month] + ' to ' + months[(month + 1) % 12] + ' ' + year);
+
             var percent = 0;
             if (funding_total > 0.0) {
-                percent = Math.floor(funding_total * 100 / data['spending_monthly']);
+                percent = Math.floor(funding_total * 100 / spending_monthly);
                 $('.funding-progressbar').append('<div class="progress-bar progress-bar-success" style="width: ' + percent + '%;"><span class="sr-only">' + percent + '% funded</span></div>');
             }
+
+            var expected_total = data['funding_monthly'];
+
             if (percent < 100) {
-                var expected_total = data['funding_monthly'];
                 data['history'].forEach(function(transaction) {
                     if (transaction['type'] == 'player-monthly') {
                         var transaction_year = transaction['date'].split('-')[0];
@@ -150,6 +153,7 @@ function display_funding_data() {
                         }
                     }
                 });
+
                 var expected_percent = Math.max(0, Math.min(100 - percent, Math.floor(expected_total * 100 / data['spending_monthly'])));
                 $('.funding-progressbar').append('<div class="progress-bar progress-bar-warning" style="width: ' + expected_percent + '%;"><span class="sr-only">' + expected_percent + '% expected</span></div>');
             }
