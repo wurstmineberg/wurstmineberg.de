7c7b3edbe3d579391718f0ef8fdd4c2918ca93c3
farthen <monkey@farthen.de>
2013-11-02 04:29:44 +0100

    Add pagination to the people statistics

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,7 +1,27 @@
+function bind_pagination_events() {
+    $('.pagination-item').bind('click', function(eventObject) {
+        var id = $(this).attr('id')
+        var elementid = id.substring('pagination-'.length, id.length);
+        var selected = $('#' + elementid);
+        $('.stats-section').each(function(index, element) {
+            var table = $(element);
+            if (table.attr('id') == selected.attr('id')) {
+                table.removeClass("hidden");
+            } else {
+                table.addClass("hidden");
+            }
+        });
+    });
+}
+
 function get_user_name() {
     var user;
     var url = document.URL;
     var username = url.substring(url.lastIndexOf("/") + 1, url.length).toLowerCase();
+    hashindex = username.lastIndexOf("#");
+    if (hashindex > 0) {
+        username = username.substring(0, username.lastIndexOf("#"));
+    };
     return username;
 }
 
@@ -75,101 +95,101 @@ function display_user_data(person) {
 }
 
 function is_block(id) {
-	return false;
+    return false;
 }
 
 function display_stat_data(data) {
-	var loading_stat_general = $('#loading-stat-general-table');
-	var loading_stat_item = $('#loading-stat-items-table');
-	var loading_stat_block = $('#loading-stat-blocks-table');
-	var loading_stat_general = $('#loading-stat-general-table');
-	var loading_stat_mobs = $('#loading-stat-mobs-table');
+    var loading_stat_general = $('#loading-stat-general-table');
+    var loading_stat_item = $('#loading-stat-items-table');
+    var loading_stat_block = $('#loading-stat-blocks-table');
+    var loading_stat_general = $('#loading-stat-general-table');
+    var loading_stat_mobs = $('#loading-stat-mobs-table');
 
-	$.each(data, function(key, value) {
-		stat = key.split('.');
-		var name;
+    $.each(data, function(key, value) {
+        stat = key.split('.');
+        var name;
 
-		if (stat[0] === 'stat') {
-			if (stat[1] === 'craftItem' ||
-				stat[1] === 'useItem' ||
-				stat[1] === 'breakItem') {
-				var item = stat[2];
-				name = item;
+        if (stat[0] === 'stat') {
+            if (stat[1] === 'craftItem' ||
+                stat[1] === 'useItem' ||
+                stat[1] === 'breakItem') {
+                var item = stat[2];
+                name = item;
 
-				var row = $('#item-row-' + item);
-				if (row.length == 0) {
-					row = '<tr id="item-row-' + item + '" class="item-row"><td class="name"></td><td class="depleted">0</td><td class="crafted">0</td><td class="used">0</td></tr>';
-					loading_stat_item.before(row);
-					row = $('#item-row-' + item);
-					row.children('.name').text(name);
-				}
+                var row = $('#item-row-' + item);
+                if (row.length == 0) {
+                    row = '<tr id="item-row-' + item + '" class="item-row"><td class="name"></td><td class="depleted">0</td><td class="crafted">0</td><td class="used">0</td></tr>';
+                    loading_stat_item.before(row);
+                    row = $('#item-row-' + item);
+                    row.children('.name').text(name);
+                }
 
-				if (row) {
-					if (stat[1] === 'craftItem') {
-						row.children('.crafted').text(value);
-					} else if (stat[1] === 'useItem') {
-						row.children('.used').text(value);
-					} else if (stat[1] === 'breakItem') {
-						row.children('.depleted').text(value);
-					}
-				}
+                if (row) {
+                    if (stat[1] === 'craftItem') {
+                        row.children('.crafted').text(value);
+                    } else if (stat[1] === 'useItem') {
+                        row.children('.used').text(value);
+                    } else if (stat[1] === 'breakItem') {
+                        row.children('.depleted').text(value);
+                    }
+                }
 
-			} else if (stat[1] === 'mineBlock') {
-				var item = stat[2];
-				name = item;
+            } else if (stat[1] === 'mineBlock') {
+                var item = stat[2];
+                name = item;
 
-				var row = $('#block-row-' + item);
-				if (row.length == 0) {
-					row = '<tr id="block-row-' + item + '" class="block-row"><td class="name"></td><td class="crafted">0</td><td class="used">0</td><td class="mined">0</td></tr>';
-					loading_stat_block.before(row);
-					row = $('#block-row-' + item);
-					row.children('.name').text(name);
-				}
+                var row = $('#block-row-' + item);
+                if (row.length == 0) {
+                    row = '<tr id="block-row-' + item + '" class="block-row"><td class="name"></td><td class="crafted">0</td><td class="used">0</td><td class="mined">0</td></tr>';
+                    loading_stat_block.before(row);
+                    row = $('#block-row-' + item);
+                    row.children('.name').text(name);
+                }
 
-				if (row) {
-					if (stat[1] === 'mineBlock') {
-						row.children('.mined').text(value);
-					}
-				}
-			} else if (stat[1] === 'killEntity' ||
-					   stat[1] === 'entityKilledBy') {
-				var mobname = stat[2];
-				var row = $('#mob-row-' + mobname);
-				if (row.length == 0) {
-					row = '<tr id="mob-row-' + mobname + '" class="mob-row"><td class="name"></td><td class="killed">0</td><td class="killed-by">0</td></tr>';
-					loading_stat_mobs.before(row);
-					row = $('#mob-row-' + mobname);
-					row.children('.name').text(mobname);
-				}
+                if (row) {
+                    if (stat[1] === 'mineBlock') {
+                        row.children('.mined').text(value);
+                    }
+                }
+            } else if (stat[1] === 'killEntity' ||
+                       stat[1] === 'entityKilledBy') {
+                var mobname = stat[2];
+                var row = $('#mob-row-' + mobname);
+                if (row.length == 0) {
+                    row = '<tr id="mob-row-' + mobname + '" class="mob-row"><td class="name"></td><td class="killed">0</td><td class="killed-by">0</td></tr>';
+                    loading_stat_mobs.before(row);
+                    row = $('#mob-row-' + mobname);
+                    row.children('.name').text(mobname);
+                }
 
-				if (stat[1] === 'killEntity') {
-					row.children('.killed').text(value);
-				} else if (stat[1] === 'entityKilledBy') {
-					row.children('.killed-by').text(value);
-				}
-			} else {
-				name = key.substring('stat.'.length, key.length);
+                if (stat[1] === 'killEntity') {
+                    row.children('.killed').text(value);
+                } else if (stat[1] === 'entityKilledBy') {
+                    row.children('.killed-by').text(value);
+                }
+            } else {
+                name = key.substring('stat.'.length, key.length);
 
 
-				var row = '<tr id="general-row-' + name + '" class="general-row"><td class="name">' + name + '</td><td class="info">' + value + '</td></tr>'
-				loading_stat_general.before(row);
-			}
-		}
-	});
+                var row = '<tr id="general-row-' + name + '" class="general-row"><td class="name">' + name + '</td><td class="info">' + value + '</td></tr>'
+                loading_stat_general.before(row);
+            }
+        }
+    });
 
-	$('.loading-stat').remove();
+    $('.loading-stat').remove();
 }
 
 function load_stat_data(minecraft) {
-	$.ajax('/assets/world/stats/' + minecraft + '.json', {
-		dataType: 'json',
-		error: function(request, status, error) {
-			$('.loading-stat').html('<td colspan="7">Error: Could not load ' + minecraft + '.json</td>');
-		},
-		success: function(data) {
-			display_stat_data(data);
-		}
-	});
+    $.ajax('/assets/world/stats/' + minecraft + '.json', {
+        dataType: 'json',
+        error: function(request, status, error) {
+            $('.loading-stat').html('<td colspan="7">Error: Could not load ' + minecraft + '.json</td>');
+        },
+        success: function(data) {
+            display_stat_data(data);
+        }
+    });
 }
 
 function load_user_data() {
@@ -185,9 +205,9 @@ function load_user_data() {
                 data.forEach(function(person) {
                     if ('id' in person) {
                         if (person['id'].toLowerCase() === username) {
-                        	if ('minecraft' in person) {
-                        		load_stat_data(person['minecraft']);
-                        	};
+                            if ('minecraft' in person) {
+                                load_stat_data(person['minecraft']);
+                            };
 
                             display_user_data(person);
                             return;
@@ -201,5 +221,5 @@ function load_user_data() {
     });
 }
 
-
+bind_pagination_events();
 load_user_data();
