1e9bf94179ab0e5474b8c2d911f4fabadd63926b
farthen <monkey@farthen.de>
2013-11-03 04:43:11 +0100

    Fix strings on stat page

--- static/js/stats.js
+++ static/js/stats.js
@@ -1,6 +1,6 @@
 function display_leaderboard_stat_data(data) {
     $.when(fetch_string_data(), fetch_player_data()).done(function(string_data, player_data) {
-        string_data = player_data[0]
+        string_data = string_data[0]
         player_data = player_data[0]
 
         var stats = {};
