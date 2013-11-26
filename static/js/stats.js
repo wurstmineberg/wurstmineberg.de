8a3905977f9053a3495be4fefb97be57bc390564
Fenhl <fenhl@fenhl.net>
2013-11-26 04:09:02 +0000

    Show players who score least on a stat

--- static/js/stats.js
+++ static/js/stats.js
@@ -21,26 +21,41 @@ function display_leaderboard_stat_data(stat_data, string_data, people) {
                     };
                 };
             }
-
+            
             $.each(stats, function(index, playerstat) {
                 if (playerstat['id'] === key) {
                     found = true;
                     if (value > playerstat['value']) {
-                        stats[index] = {'id': key, 'name': name, 'players': [player], 'value': value};
-                        return;
+                        stats[index]['players'] = [player];
+                        stats[index]['value'] = value;
                     } else if (value == playerstat['value']) {
                         stats[index]['players'].push(player);
+                    }
+                    if (value < playerstat['minvalue']) {
+                        stats[index]['minplayers'] = [player];
+                        stats[index]['minvalue'] = value;
+                    } else if (value == playerstat['minvalue']) {
+                        stats[index]['minplayers'].push(player);
+                    }
+                    if (found) {
                         return;
                     }
                 }
             });
-
+            
             if (!found) {
-                stats.push({'id': key, 'name': name, 'players': [player], 'value': value});
+                stats.push({
+                    'id': key,
+                    'name': name,
+                    'players': [player],
+                    'value': value,
+                    'minplayers': [player],
+                    'minvalue': value
+                });
             };
         });
     });
-
+    
     stats.sort(function(a, b) {
         nameA = a['name'];
         nameB = b['name'];
@@ -54,9 +69,12 @@ function display_leaderboard_stat_data(stat_data, string_data, people) {
 
         var players = data['players'];
         var playerhtml = html_player_list(players);
+        var minplayers = data['minplayers'];
+        var minplayerhtml = html_player_list(players);
         var value = prettify_stats_value(stat[1], data['value']);
+        var minvalue = prettify_stats_value(stat[1], data['minvalue']);
 
-        row = '<tr class="leaderboard-row"><td class="stat">' + name + '</td><td class="leading-player">' + playerhtml + '</td><td class="value">' + value + '</td></tr>';
+        row = '<tr class="leaderboard-row"><td class="stat">' + name + '</td><td class="leading-player">' + playerhtml + '</td><td class="value">' + value + '</td><td class="trailing-player">' + minplayerhtml + '</td><td class="minvalue">' + minvalue + '</td></tr>';
         loading_leaderboards.before(row);
     });
 
@@ -64,13 +82,13 @@ function display_leaderboard_stat_data(stat_data, string_data, people) {
 }
 
 function load_leaderboard_stat_data() {
-	$.when(API.statData(), API.stringData(), API.people())
-		.done(function(stat_data, string_data, people) {
-			display_leaderboard_stat_data(stat_data, string_data, people)
-		})
-		.fail(function() {
+    $.when(API.statData(), API.stringData(), API.people())
+        .done(function(stat_data, string_data, people) {
+            display_leaderboard_stat_data(stat_data, string_data, people)
+        })
+        .fail(function() {
             $('.loading-stat').html('<td colspan="7">Error: Could not load api.wurstmineberg.de/server/playerstats/general.json</td>');
-		});
+        });
 }
 
 load_leaderboard_stat_data();
