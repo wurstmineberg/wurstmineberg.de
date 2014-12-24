e8e51380a1a7e122871cbcd0fad48ca1b138fc6a
Fenhl <fenhl@fenhl.net>
2014-12-24 15:19:38 +0000

    Cleanup

--- wurstmineberg-web.py
+++ wurstmineberg-web.py
@@ -1,69 +1,59 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Wurstmineberg Website
+Wurstmineberg website
 """
-
 
 import bottle
-import os
+import os.path
 
-app = application = bottle.Bottle()  # aliased as application for uwsgi to find
+application = bottle.Bottle() # aliased as application for uwsgi to find
 
-workingDirectory = os.path.dirname(__file__)
+working_directory = os.path.dirname(__file__)
 bottle.TEMPLATE_PATH = [
-    os.path.join(workingDirectory, 'views/includes'),
-    os.path.join(workingDirectory, 'views')]
+    os.path.join(working_directory, 'views/includes'),
+    os.path.join(working_directory, 'views')
+]
 
+include_files = ['footer', 'header', 'navigation', 'singleservingfooter']
+template_variables = {}
 
-includeFiles = ['footer', 'header', 'navigation', 'singleservingfooter']
-templateVariables = {}
+for name in include_files:
+    with open(os.path.join(working_directory, 'views/includes', name + '.html')) as file:
+        template_variables[name] = file.read()
 
-for name in includeFiles:
-    with open(os.path.join(workingDirectory, 'views/includes', name + '.html'), encoding='utf8') as file:
-        templateVariables[name] = file.read()
-
-
-@app.route('/')
+@application.route('/')
 @bottle.view('index')
 def index():
-    return templateVariables
+    return template_variables
 
-
-@app.route('/about')
+@application.route('/about')
 @bottle.view('about')
 def index():
-    return templateVariables
+    return template_variables
 
-
-@app.route('/stats')
+@application.route('/stats')
 @bottle.view('stats')
 def index():
-    return templateVariables
+    return template_variables
 
-
-@app.route('/people')
+@application.route('/people')
 @bottle.view('people')
 def index():
-    return templateVariables
+    return template_variables
 
-
-@app.route('/people/:person')
-@bottle.view('peopleDetail')
+@application.route('/people/<person:re:[a-z][0-9a-z]{1,15}>')
+@bottle.view('people_detail')
 def index(person):
-    return templateVariables
-
+    return template_variables
 
 class StripPathMiddleware:
-
     """Get that slash out of the request"""
-
     def __init__(self, a):
         self.app = a
-
+    
     def __call__(self, e, h):
         e['PATH_INFO'] = e['PATH_INFO'].rstrip('/')
         return self.app(e, h)
 
 if __name__ == '__main__':
-    bottle.run(app=StripPathMiddleware(app), host='0.0.0.0', port=8081)
+    bottle.run(app=StripPathMiddleware(application), host='0.0.0.0', port=8081)
