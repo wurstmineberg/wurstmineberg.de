633b544d655f7e8266835cc37a2992fc62069ed9
Fenhl <fenhl@fenhl.net>
2019-03-05 07:01:05 +0000

    Report internal server errors

--- wurstmineberg_web/error.py
+++ wurstmineberg_web/error.py
@@ -1,8 +1,29 @@
 import flask
 import io
+import subprocess
 import traceback
 
 from wurstmineberg_web import app
+
+CRASH_NOTICE = """To: root@wurstmineberg.de
+From: {whoami}@{hostname}
+Subject: wurstmineberg.de internal server error
+
+An internal server error occurred on wurstmineberg.de.
+User: {user}
+"""
+
+def notify_crash(exc=None):
+    whoami = subprocess.run(['whoami'], stdout=subprocess.PIPE, check=True).stdout.decode('utf-8').strip()
+    hostname = subprocess.run(['hostname', '-f'], stdout=subprocess.PIPE, check=True).stdout.decode('utf-8').strip()
+    try:
+        user = str(flask.g.user)
+    except Exception:
+        user = None
+    mail_text = CRASH_NOTICE.format(whoami=whoami, hostname=hostname, user=user)
+    if exc is not None:
+        mail_text += '\n' + traceback.format_exc()
+    return subprocess.run(['ssmtp', 'root@wurstmineberg.de'], input=mail_text.encode('utf-8'), check=True)
 
 @app.errorhandler(403)
 @app.errorhandler(404)
@@ -14,5 +35,12 @@ def error_handler(error):
     except AttributeError:
         code = 500
     report = code == 500
-    flask.flash(flask.Markup.escape(str(error)), 'error')
-    return flask.render_template('error.html', error=error, is_exception=lambda v: isinstance(v, Exception), report=report, traceback=traceback), code
+    reported = False
+    if report:
+        try:
+            notify_crash(error)
+        except Exception:
+            pass
+        else:
+            reported = True
+    return flask.render_template('error.html', error=error, is_exception=lambda v: isinstance(v, Exception), report=report, reported=reported, traceback=traceback), code
