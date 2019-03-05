0a5ac3eff112626999b4f985f8e8f5d4f8c26154
Fenhl <fenhl@fenhl.net>
2019-03-05 04:41:13 +0000

    Fix import in database module

--- wurstmineberg_web/database.py
+++ wurstmineberg_web/database.py
@@ -27,7 +27,7 @@ def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
-    from wurstmineberg.models import Person
+    from wurstmineberg_web.models import Person
 
     Base.metadata.create_all(bind=engine)
 
