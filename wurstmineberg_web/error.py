7ac474972c2d1de579f1dadd94c43b984959fdd5
Fenhl <fenhl@fenhl.net>
2020-04-09 00:12:36 +0000

    Update crash notices to use night with fallback to new mail system

--- wurstmineberg_web/error.py
+++ wurstmineberg_web/error.py
@@ -3,21 +3,15 @@ import io
 import subprocess
 import traceback
 
-from wurstmineberg_web import app
+import wurstmineberg_web
 import wurstmineberg_web.util
 
-CRASH_NOTICE = """To: root@wurstmineberg.de
-From: {whoami}@{hostname}
-Subject: wurstmineberg.de internal server error
-
-An internal server error occurred on wurstmineberg.de.
+CRASH_NOTICE = """An internal server error occurred on wurstmineberg.de.
 User: {user}
 URL: {url}
-"""
+{tb}"""
 
-def notify_crash(exc=None):
-    whoami = subprocess.run(['whoami'], stdout=subprocess.PIPE, check=True).stdout.decode('utf-8').strip()
-    hostname = subprocess.run(['hostname', '-f'], stdout=subprocess.PIPE, check=True).stdout.decode('utf-8').strip()
+def notify_crash():
     try:
         user = str(flask.g.user)
     except Exception:
@@ -26,15 +20,19 @@ def notify_crash(exc=None):
         url = str(flask.g.view_node.url)
     except Exception:
         url = None
-    mail_text = CRASH_NOTICE.format(whoami=whoami, hostname=hostname, user=user, url=url)
-    if exc is not None:
-        mail_text += '\n' + traceback.format_exc()
-    return subprocess.run(['ssmtp', 'root@wurstmineberg.de'], input=mail_text.encode('utf-8'), check=True)
+    exc_text = CRASH_NOTICE.format(user=user, url=url, tb=traceback.format_exc())
+    try:
+        # notify night
+        response = requests.post('https://nightd.fenhl.net/wurstmineberg/crash', json={'text': exc_text}, auth=('wurstmineberg', wurstmineberg_web.app.config['night']['password']))
+        response.raise_for_status()
+        return
+    except Exception:
+        subprocess.run(['mail', '-s', 'wurstmineberg.de internal server error', 'root@wurstmineberg.de'], input=exc_text.encode('utf-8'), check=True)
 
-@app.errorhandler(403)
-@app.errorhandler(404)
-@app.errorhandler(410)
-@app.errorhandler(500)
+@wurstmineberg_web.app.errorhandler(403)
+@wurstmineberg_web.app.errorhandler(404)
+@wurstmineberg_web.app.errorhandler(410)
+@wurstmineberg_web.app.errorhandler(500)
 def error_handler(error):
     try:
         code = error.code
@@ -44,7 +42,7 @@ def error_handler(error):
     reported = False
     if report:
         try:
-            notify_crash(error)
+            notify_crash()
         except Exception:
             pass
         else:
