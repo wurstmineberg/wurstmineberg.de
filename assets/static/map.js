7b0afa95552f0752025300c9bf50af7fae9daab7
Fenhl <fenhl@fenhl.net>
2025-02-28 23:44:59 +0000

    map: Try to fix zoom

--- assets/static/map.js
+++ assets/static/map.js
@@ -1,11 +1,12 @@
 var map = L.map('map', {
     crs: L.CRS.Simple,
-    minZoom: -4,
-    maxZoom: 4,
 })
 .setView([8192, -4096], 0);
 L.tileLayer('https://map.wurstmineberg.de/r.{x}.{y}.png', {
+    tileSize: 512,
     detectRetina: true,
-    minZoom: 0,
-    maxZoom: 0,
+    minZoom: -4,
+    maxZoom: 4,
+    minNativeZoom: 0,
+    maxNativeZoom: 0,
 }).addTo(map);
