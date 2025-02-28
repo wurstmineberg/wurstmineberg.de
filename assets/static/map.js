b77787ca22acb8e4647597aa32f0bb30d559c00b
Fenhl <fenhl@fenhl.net>
2025-02-28 22:46:35 +0000

    map: Translate Zucchini coords to Leaflet coord system

--- assets/static/map.js
+++ assets/static/map.js
@@ -1,7 +1,7 @@
 var map = L.map('map', {
     crs: L.CRS.Simple,
 })
-.setView([-16, -32], 0);
+.setView([8192, -4096], 0);
 L.tileLayer('https://map.wurstmineberg.de/r.{x}.{y}.png', {
     detectRetina: true,
     minZoom: 0,
