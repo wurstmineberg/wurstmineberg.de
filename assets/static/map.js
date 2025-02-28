4ff0cafedcc4feb20e3d4f3db4ddf301160d21de
Fenhl <fenhl@fenhl.net>
2025-02-28 22:41:32 +0000

    map: Start centered on Zucchini

--- assets/static/map.js
+++ assets/static/map.js
@@ -1,7 +1,7 @@
 var map = L.map('map', {
     crs: L.CRS.Simple,
 })
-.setView([0, 0], 0);
+.setView([-16, -32], 0);
 L.tileLayer('https://map.wurstmineberg.de/r.{x}.{y}.png', {
     detectRetina: true,
     minZoom: 0,
