f86ab931ed4c28e947f346b7e50556e95d481619
Fenhl <fenhl@fenhl.net>
2025-02-28 22:19:49 +0000

    Directly load map tiles from map.wurstmineberg.de for now

--- assets/static/map.js
+++ assets/static/map.js
@@ -2,6 +2,6 @@ var map = L.map('map', {
     crs: L.CRS.Simple,
 })
 .setView([0, 0], 0);
-L.tileLayer('https://wurstmineberg.de/api/v3/world/wurstmineberg/map/{z}/{x}/{y}.png', {
+L.tileLayer('https://map.wurstmineberg.de/r.{x}.{y}.png', {
     detectRetina: true,
 }).addTo(map);
