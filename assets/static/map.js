8476e57141393cff343843a3123e1e23c8107c6e
Fenhl <fenhl@fenhl.net>
2025-02-28 23:56:01 +0000

    map: Fix starting coords again and allow zooming out further

--- assets/static/map.js
+++ assets/static/map.js
@@ -1,12 +1,12 @@
 var map = L.map('map', {
     crs: L.CRS.Simple,
 })
-.setView([8192, -4096], 0);
+.setView([16384, -8192], 0);
 L.tileLayer('https://map.wurstmineberg.de/r.{x}.{y}.png', {
     tileSize: 512,
     detectRetina: true,
     minZoom: -4,
-    maxZoom: 4,
+    maxZoom: 18,
     minNativeZoom: 0,
     maxNativeZoom: 0,
 }).addTo(map);
