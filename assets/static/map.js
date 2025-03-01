410269f5d301b81e7512f21cec96e14c1f27af8c
Fenhl <fenhl@fenhl.net>
2025-03-01 00:54:20 +0000

    map: Disable detectRetina due to bugs

--- assets/static/map.js
+++ assets/static/map.js
@@ -4,7 +4,7 @@ var map = L.map('map', {
 .setView([16384, -8192], 0);
 L.tileLayer('https://map.wurstmineberg.de/r.{x}.{y}.png', {
     tileSize: 512,
-    detectRetina: true,
+    detectRetina: false, // breaks the map when zooming in or out too far
     minZoom: -4, // should not be decreased without also decreasing min native zoom
     maxZoom: 4,
     minNativeZoom: 0,
