06481ec87ad3bd1c2771283ae6600d5cebbc7c2b
Fenhl <fenhl@fenhl.net>
2025-11-24 02:00:24 +0000

    Fix deploy script

--- assets/deploy.ps1
+++ assets/deploy.ps1
@@ -4,32 +4,44 @@ if (-not $?)
     throw 'Native Failure'
 }
 
+wsl -d ubuntu-m2 sudo -n apt-get install -y musl-tools
+if (-not $?)
+{
+    throw 'Native Failure'
+}
+
 # copy the tree to the WSL file system to improve compile times
-wsl rsync --delete -av /mnt/c/Users/fenhl/git/github.com/wurstmineberg/wurstmineberg.de/stage/ /home/fenhl/wslgit/github.com/wurstmineberg/wurstmineberg.de/ --exclude target
+wsl -d ubuntu-m2 rsync --mkpath --delete -av /mnt/c/Users/fenhl/git/github.com/wurstmineberg/wurstmineberg.de/stage/ /home/fenhl/wslgit/github.com/wurstmineberg/wurstmineberg.de/ --exclude target
 if (-not $?)
 {
     throw 'Native Failure'
 }
 
-wsl rustup update stable
+wsl -d ubuntu-m2 /home/fenhl/.cargo/bin/rustup update stable
 if (-not $?)
 {
     throw 'Native Failure'
 }
 
-wsl env -C /home/fenhl/wslgit/github.com/wurstmineberg/wurstmineberg.de cargo build --release --target=x86_64-unknown-linux-musl
+wsl -d ubuntu-m2 /home/fenhl/.cargo/bin/rustup target add x86_64-unknown-linux-musl
 if (-not $?)
 {
     throw 'Native Failure'
 }
 
-wsl mkdir -p /mnt/c/Users/fenhl/git/github.com/wurstmineberg/wurstmineberg.de/stage/target/wsl/release
+wsl -d ubuntu-m2 env -C /home/fenhl/wslgit/github.com/wurstmineberg/wurstmineberg.de /home/fenhl/.cargo/bin/cargo build --release --target=x86_64-unknown-linux-musl
 if (-not $?)
 {
     throw 'Native Failure'
 }
 
-wsl cp /home/fenhl/wslgit/github.com/wurstmineberg/wurstmineberg.de/target/x86_64-unknown-linux-musl/release/wurstmineberg-web /mnt/c/Users/fenhl/git/github.com/wurstmineberg/wurstmineberg.de/stage/target/wsl/release/wurstmineberg-web
+wsl -d ubuntu-m2 mkdir -p /mnt/c/Users/fenhl/git/github.com/wurstmineberg/wurstmineberg.de/stage/target/wsl/release
+if (-not $?)
+{
+    throw 'Native Failure'
+}
+
+wsl -d ubuntu-m2 cp /home/fenhl/wslgit/github.com/wurstmineberg/wurstmineberg.de/target/x86_64-unknown-linux-musl/release/wurstmineberg-web /mnt/c/Users/fenhl/git/github.com/wurstmineberg/wurstmineberg.de/stage/target/wsl/release/wurstmineberg-web
 if (-not $?)
 {
     throw 'Native Failure'
