44af89804d58f1946c1d9263602ee098fd52aa90
Fenhl <fenhl@fenhl.net>
2026-06-12 02:00:04 +0000

    deploy: Build on gharch

--- assets/deploy.sh
+++ assets/deploy.sh
@@ -5,17 +5,19 @@ set -e
 if [[ x"$(hostname -f)" == x'gharch.wurstmineberg.de' ]]; then
     # deploy wurstmineberg.de
     echo 'deploying wurstmineberg.de'
+    rustup update stable
+    env -C /opt/git cargo sweep -ir
     cd /opt/git/github.com/wurstmineberg/wurstmineberg.de/main
-    git --git-dir=/opt/git/github.com/wurstmineberg/wurstmineberg.de/main/.git pull
+    sudo -u wurstmineberg git --git-dir=/opt/git/github.com/wurstmineberg/wurstmineberg.de/main/.git pull
+    cargo build --release
     sudo systemctl stop wurstmineberg-web
-    mv /opt/wurstmineberg/bin/wurstmineberg-web-next /opt/wurstmineberg/bin/wurstmineberg-web
+    sudo chown wurstmineberg:wurstmineberg target/release/wurstmineberg-web
+    sudo mv target/release/wurstmineberg-web /opt/wurstmineberg/bin/wurstmineberg-web
     sudo systemctl start wurstmineberg-web
     # reload caddy (since caddy config is tracked by git) and uWSGI
     sudo systemctl daemon-reload
     sudo systemctl reload caddy nginx uwsgi
 else
     git push
-    cargo build --release
-    scp target/release/wurstmineberg-web wurstmineberg.de:/opt/wurstmineberg/bin/wurstmineberg-web-next
     ssh wurstmineberg.de /opt/git/github.com/wurstmineberg/wurstmineberg.de/main/assets/deploy.sh
 fi
